fb0a332dce5195edc4193b01421be748d9cdf1bf
Edson Copque <ecop@disroot.org>
2024-09-30 19:35:38 -0300

    Creating the product and variation models

--- product/models.py
+++ product/models.py
@@ -1,3 +1,20 @@
+# FILE: product/models.py
+
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    short_description = models.TextField(max_length=255)
+    long_description = models.TextField()
+    image = models.ImageField(upload_to='product_images/%Y/%m', blank=True, null=True)
+    slug = models.SlugField(unique=True)
+    marketing_price = models.FloatField()
+    promotional_marketing_price = models.FloatField(default=0)
+    type = models.CharField(default='V', max_length=1, choices=(
+        ('V', 'Variation'),
+        ('S', 'Simple'),
+        ),
+    )
+
+
+# https://linktr.ee/edsoncopque
