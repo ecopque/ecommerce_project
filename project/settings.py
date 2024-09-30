1cc9fd31b416db5b4fd375935873b47bf58bf76d
Edson Copque <ecop@disroot.org>
2024-09-30 19:39:49 -0300

    Creating the product and variation models

--- project/settings.py
+++ project/settings.py
@@ -41,8 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'debug_toolbar', #2:
+    'product', ##
 ]
 
 MIDDLEWARE = [
