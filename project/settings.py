21929bcb02afe13712c49c19cad3960c690f8ef0
Edson Copque <ecop@disroot.org>
2024-09-29 21:02:18 -0300

    Creating the e-commerce project

--- project/settings.py
+++ project/settings.py
@@ -135,3 +135,5 @@ MESSAGE_TAGS = { ##
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# https://linktr.ee/edsoncopque
