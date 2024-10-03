dfc3cf26708e88057a7741b413ded788d9609b29
Edson Copque <ecop@disroot.org>
2024-10-03 11:01:01 -0300

    Creating the \order\ and \item_order\ models

--- order/models.py
+++ order/models.py
@@ -1,6 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE) ##
+    total = models.FloatField() ##
+    status = models.CharField(
+        default='C',
+        max_length=1,
+        choices=(
+            ('A', 'Approved'),
+            ('C', 'Created'),
+            ('R', 'Rejected'),
+            ('P', 'Pending'),
+            ('S', 'Sent'),
+            ('F', 'Finalized'),
+        )
+    ) ##
 
 
 # https://linktr.ee/edsoncopque
