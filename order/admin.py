73a2c284fc50da3ba934d1a4454e3eccd6775eed
Edson Copque <ecop@disroot.org>
2024-10-03 17:06:47 -0300

    Creating the ‘order’ and ‘item_order’ models

--- order/admin.py
+++ order/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from models import Order, OrderItem
+from order.models import Order, OrderItem
+
 
 class OrderItemInline(admin.TabularInline): ##
     model = OrderItem ##
