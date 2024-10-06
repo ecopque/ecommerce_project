00002c1b4559c2e893d3b17cec996e3930efd17b
Edson Copque <ecop@disroot.org>
2024-10-05 23:14:34 -0300

    Automatic slug and product registration

--- product/admin.py
+++ product/admin.py
@@ -15,13 +15,14 @@ class ProductAdmin(admin.ModelAdmin): #4:
 
     # URL⬇: http://127.0.0.1:8000/admin/product/product/
     # IMPORT⬇: /product/models.py
-    list_display = ['name', 'marketing_price', 'get_formatted_price', 'price_promotional_marketing', 'type'] ##
+    list_display = ['name', 'marketing_price', 'get_formatted_price', 'price_promotional_marketing', 'type'] #8:
 
 admin.site.register(Product, ProductAdmin) #6:
 admin.site.register(Variation) #7:
 
 
-string = 'That\'s a nice idea'
+# ------------------------------------------------------------------
+#8: No Django Admin, o atributo list_display define quais campos e métodos da classe Product serão exibidos na lista de produtos. Aqui, os campos name, marketing_price, price_promotional_marketing, e type são exibidos, junto com o método get_formatted_price. get_formatted_price está sendo solicitado diretamente no painel administrativo do Django para exibir o preço formatado.
 # ------------------------------------------------------------------
 #1: Define uma classe de configuração do Django admin para exibir variações dentro da página de edição de um produto. TabularInline permite editar múltiplas variações diretamente na página do produto.
 #2: Especifica que o modelo Variation será exibido dentro do admin como parte de um produto.
