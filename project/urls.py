febb090cf5dabc1b21f47084ec44b77d86a5bfdf
Edson Copque <ecop@disroot.org>
2024-10-07 15:51:19 -0300

    URLs

--- project/urls.py
+++ project/urls.py
@@ -1,22 +1,5 @@
 # FILE: /project/urls.py
 
-
-"""
-URL configuration for project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -32,7 +15,6 @@ if settings.DEBUG: #3:
         path('__debug__/', include(debug_toolbar.urls)), #5:
     ] + urlpatterns
 
-# https://linktr.ee/edsoncopque
 
 # ------------------------------------------------------------------
 #1: Importa a função static de django.conf.urls. Ela é usada para servir arquivos de mídia em um ambiente de desenvolvimento.
@@ -40,3 +22,5 @@ if settings.DEBUG: #3:
 #3: Verifica se o modo de depuração (DEBUG) está ativado. Se estiver, a lógica abaixo será executada, permitindo a ativação de ferramentas como o Django Debug Toolbar.
 #4: Importa o pacote debug_toolbar, necessário para que a barra de ferramentas de depuração seja incluída nas URLs e exibida no navegador.
 #5: Adiciona uma rota (__debug__/) para o Django Debug Toolbar. Quando o modo de depuração está ativado, essa rota permite acessar as ferramentas de depuração diretamente no navegador.
+
+# https://linktr.ee/edsoncopque
