33869cac6f68135da6d1ebe4eb3b2244483e36ad
Edson Copque <ecop@disroot.org>
2024-10-07 23:00:12 -0300

    URLs

--- order/urls.py
+++ order/urls.py
@@ -1,9 +1,12 @@
-#FILE: /order/urls.py
+# FILE: /order/urls.py
 
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path('', views.Pay.as_view(), name='pay'),
+    path('close_order', views.Close_Order.as_view(), name='close_order'),
+    path('details', views.Details.as_view(), name='details'),
 ]
 
 
--- order/views.py
+++ order/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+# FILE: /order/views.py
 
-# Create your views here.
+from django.shortcuts import render
+from django.views.generic import ListView
+from django.views import View
+
+class Pay(ListView):
+    ...
+
+class Close_Order(View):
+    ...
+
+class Details(View):
+    ...
