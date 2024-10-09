06cefe33f3775158a6db25a6e8dd52cd6b3bc3b6
Edson Copque <ecop@disroot.org>
2024-10-09 15:37:28 -0300

    Listing products (templates)

--- product/templatetags/omfilter.py
+++ product/templatetags/omfilter.py
@@ -1,5 +1,10 @@
 # FILE: /product/templatetags/omfilter.py
 
+from django.template import Library
 
+register = Library() ##
+
+def format_price(value):
+    return f'R${value:.2}'
 
 # https://linktr.ee/edsoncopque
