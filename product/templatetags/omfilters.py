d4bfa7111976a9504ec98e90bd6bbde90165f5b2
Edson Copque <ecop@disroot.org>
2024-10-11 18:10:36 -0300

    Listing products (templates)

--- product/templatetags/omfilters.py
+++ product/templatetags/omfilters.py
@@ -3,11 +3,16 @@
 from django.template import Library
 from utils import utils
 
-register = Library() ##
+register = Library() #1:
 
-@register.filter
-def format_price(value): ##
-    return utils.format_pricevrs(value) ##
+@register.filter #2:
+def format_price(value): #3:
+    return utils.format_pricevrs(value) #3:
 
+
+# ------------------------------------------------------------------
+#1: Cria uma instância de Library para registrar filtros personalizados. Lógica: Essa linha é necessária para permitir que você adicione filtros personalizados no sistema de templates do Django.
+#2: Um decorador que registra a função format_price como um filtro de template. Lógica: O decorador permite que a função seja usada nos templates do Django como um filtro aplicado em variáveis, por exemplo, {{ price|format_price }}.
+#3: Chama a função format_pricevrs no módulo utils para formatar o valor. Lógica: A separação da lógica de formatação para o arquivo utils.py mantém o código modular e reutilizável.
 
 # https://linktr.ee/edsoncopque
--- utils/utils.py
+++ utils/utils.py
@@ -1,7 +1,11 @@
 # FILE: /utils/utils.py
 
-def format_pricevrs(value): ##
-    return f'₿{value:.2f}'.replace('.', ',') ##
+def format_pricevrs(value): #1:
+    return f'₿{value:.2f}'.replace('.', ',') #2:
 
+
+# ------------------------------------------------------------------
+#1: Declara a função que formata o valor recebido. Lógica: Esta função converte um número para o formato de preço em Bitcoin, substituindo o ponto decimal por uma vírgula.
+#2: Retorna o valor formatado como uma string de Bitcoin, com duas casas decimais, e substitui o ponto decimal por uma vírgula. Lógica: A formatação é feita para seguir o estilo comum em alguns países (ex.: Brasil), onde a vírgula é usada como separador decimal.
 
 # https://linktr.ee/edsoncopque
