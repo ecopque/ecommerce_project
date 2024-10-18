eabae0915105efb69486ac5349326bf6aa43b69a
Edson Copque <ecop@disroot.org>
2024-10-18 15:55:33 -0300

    User registration

--- client_profile/forms.py
+++ client_profile/forms.py
@@ -1,3 +1,25 @@
 # FILE: /client_profile/forms.py
 
+from django import forms
+from . import models
+from django.contrib.auth.models import User
+
+class PerfilForm(forms.ModelForm): ##
+    class Meta:
+        #IMPORT⬇: /client_profile/models.py
+        model = models.Client_Profile ##
+        fields = '__all__' ##
+        exclude = ('user',) ##
+
+    class UserForm(forms.ModelForm): ##
+        class Meta:
+            model = User ##
+            fields = ('first_name', 'last_name', 'username', 'password', 'email') ##
+
+        def clean(self, *args, **kwargs): ##
+            data = self.data ##
+            cleaned = self.cleaned_data ##
+
+
+
 # https://linktr.ee/edsoncopque
