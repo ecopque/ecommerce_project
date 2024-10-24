6b22df01f221349e1a036e01debaf9129fe1922b
Edson Copque <ecop@disroot.org>
2024-10-24 16:18:08 -0300

    Saving the order

--- order/views.py
+++ order/views.py
@@ -28,7 +28,7 @@ class Pay(View):
             cart_variation_ids.append(v)
         print(cart_variation_ids)
 
-        bd_variations = list(Variation.objects.filter(id__in=cart_variation_ids)) #4: ##
+        bd_variations = list(Variation.objects.select_related('product').filter(id__in=cart_variation_ids)) #4: ##
         print(bd_variations)
 
 
@@ -44,7 +44,7 @@ class Details(View):
 #1: Se não estiver logado e tentar acessar '127.0.0.1:8000/order/', será redirecionado para a página de 'create' e receberá uma mensagem de erro. ;
 #2: Mesmo se estiver logado e tentar acessar '127.0.0.1:8000/order/' e não houver itens no carrinho, será redirecionado para 'list' e receberá mensagem de erro;
 #3: Aqui estamos obtendo as chaves, em nosso caso as 'IDs' para trabalhar a questão se tem estoque quando cliente for emitir o pedido;
-#4: Aqui estamos tendo acesso [selecionando] às variações;
+#4: Aqui estamos tendo acesso [selecionando] às variações. Este 'select_related' foi para diminuir outras consultas que estavam aparecendo no relatório do Django;
 
 
 # https://linktr.ee/edsoncopque
