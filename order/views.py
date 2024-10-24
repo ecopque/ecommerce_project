568b032ddd0cb033f435f5768c7afb989149557e
Edson Copque <ecop@disroot.org>
2024-10-24 16:15:41 -0300

    Saving the order

--- order/views.py
+++ order/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, redirect
 from django.views.generic import ListView
 from django.views import View
 from django.contrib import messages
+from product.models import Variation
 
 class Pay(View):
     # IMPORT⬇: /order/templates/order/pay.html
@@ -19,13 +20,17 @@ class Pay(View):
             return redirect('product:list')
         
         cart = self.request.session.get('cart') #3: ##
-        # cart_variation_id = [v for v in cart]
-        # print(cart_variation_id)
+        # cart_variation_ids = [v for v in cart]
+        # print(cart_variation_ids)
         
-        cart_variation_id = []
+        cart_variation_ids = []
         for v in cart: ##
-            cart_variation_id.append(v)
-        print(cart_variation_id)
+            cart_variation_ids.append(v)
+        print(cart_variation_ids)
+
+        bd_variations = list(Variation.objects.filter(id__in=cart_variation_ids)) #4: ##
+        print(bd_variations)
+
 
         context = {}
         return render(self.request, self.template_name, context) ##
@@ -38,7 +43,8 @@ class Details(View):
 
 #1: Se não estiver logado e tentar acessar '127.0.0.1:8000/order/', será redirecionado para a página de 'create' e receberá uma mensagem de erro. ;
 #2: Mesmo se estiver logado e tentar acessar '127.0.0.1:8000/order/' e não houver itens no carrinho, será redirecionado para 'list' e receberá mensagem de erro;
-#3: Aqui estamos obtendo as chaves, em nosso caso as 'IDs';
+#3: Aqui estamos obtendo as chaves, em nosso caso as 'IDs' para trabalhar a questão se tem estoque quando cliente for emitir o pedido;
+#4: Aqui estamos tendo acesso [selecionando] às variações;
 
 
 # https://linktr.ee/edsoncopque
