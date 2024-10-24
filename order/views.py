858175ee64d2d598ea87f641afc1488ec0baa375
Edson Copque <ecop@disroot.org>
2024-10-24 14:51:13 -0300

    Saving the order

--- order/views.py
+++ order/views.py
@@ -1,11 +1,17 @@
 # FILE: /order/views.py
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView
 from django.views import View
+from django.contrib import messages
 
 class Pay(ListView):
-    ...
+    # IMPORT⬇: /order/templates/order/pay.html
+    template_name = 'order/pay.html' ##
+
+    def get(self, *args, **kwargs):
+        context = {}
+        return redirect(self.request, self.template_name, context) ##
 
 class SaveOrder(View):
     ...
