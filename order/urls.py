1739ff359e54aa5a591c2c313cfbc6d94f381cf0
Edson Copque <ecop@disroot.org>
2024-10-30 14:30:47 -0300

    List and order detail for user

--- order/urls.py
+++ order/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('pay/<int:pk>', views.Pay.as_view(), name='pay'), #
     path('save_order/', views.SaveOrder.as_view(), name='save_order'),
     path('list/', views.List.as_view(), name='list'),
-    path('details/', views.Details.as_view(), name='details'),
+    path('details/<int:pk>', views.Details.as_view(), name='details'),
 ]
 
 
